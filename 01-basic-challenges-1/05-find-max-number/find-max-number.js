528c3f4523c79d37fbd41cc1a8dd08e672926e52
Plamen Petrov <biznetmen@gmail.com>
2023-09-28 12:21:54 +0300

    run 01-05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,10 @@
-function findMaxNumber() {}
-
+function findMaxNumber(arr) {
+    let maxNumber = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxNumber) {
+            maxNumber = arr[i];
+        }
+    }
+    return maxNumber;
+}
 module.exports = findMaxNumber;
