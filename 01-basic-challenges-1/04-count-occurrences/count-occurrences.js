4832fb4f9c2d8390e5291aa0d3edd4d19f75d714
Plamen Petrov <biznetmen@gmail.com>
2023-09-28 12:13:04 +0300

    run 01-02

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,10 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (char === str[i]) {
+            result++;
+        }
 
+    } return result;
+}
 module.exports = countOccurrences;
