0e86e37b66ada35fb322a0944eaf8020edd23de3
Plamen Petrov <biznetmen@gmail.com>
2023-09-28 11:52:26 +0300

    run 01-02

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,11 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    switch (operator) {
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '/': return num1 / num2;
+        case '*': return num1 * num2;
+
+    }
+}
 
 module.exports = calculator;
