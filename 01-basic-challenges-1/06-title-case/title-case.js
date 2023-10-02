c7b55604d6bbcb1ef3780fe581f8fe0b84d7696c
Plamen Petrov <biznetmen@gmail.com>
2023-10-02 12:22:45 +0300

    run 01-06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+function titleCase(string) {
+    const wordsArray = string.split(' ');
+    let resultArray = [];
+    let newWord = '';
+    wordsArray.forEach(element => {
+        newWord = element[0].toUpperCase() + element.slice(1);
+        resultArray.push(newWord);
+    });
+    result = resultArray.join(' ')
+    return result
+}
 
 module.exports = titleCase;
