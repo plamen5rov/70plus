e8b880bdfe44746a4589793fb49fca556be19a4d
Plamen Petrov <biznetmen@gmail.com>
2023-10-03 11:45:47 +0300

    run 01-08

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,9 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    let reversedString = str.split('').reverse().join('');
+    // console.log(reversedString);
+    return str == reversedString;
+}
+
+
 
 module.exports = isPalindrome;
