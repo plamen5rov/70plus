697dc8db265cb2c998e1479fb10810a32a32ae42
Plamen Petrov <biznetmen@gmail.com>
2023-10-04 09:56:19 +0300

    run 01-09

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,7 +1,8 @@
 function countVowels(str) {
     const vowels = ["a", "e", "i", "o", "u"];
+    let string = str.toLowerCase();
     let count = 0;
-    for (const char of str) {
+    for (const char of string) {
         if (vowels.includes(char)) {
             count++;
         }
