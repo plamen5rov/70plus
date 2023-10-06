345782f7504dab65ae67faa1379c99ff972d3e0d
Plamen Petrov <biznetmen@gmail.com>
2023-10-06 17:05:19 +0300

    run 02-02

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    let arr1Set = new Set(arr1);
+    let arr3 = []
 
+    for (let item of arr2) {
+        if (arr1Set.has(item)) {
+            arr3.push(item);
+        }
+
+
+    } return arr3;
+}
 module.exports = arrayIntersection;
