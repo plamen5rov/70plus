bf0858ade54b087ab09fa9c2c4114f9e9113645b
Plamen Petrov <biznetmen@gmail.com>
2023-10-07 10:02:55 +0300

    run 02-03

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,24 @@
-function displayLikes() {}
+function displayLikes(arr) {
+    let arrLength = arr.length;
+
+    switch (arrLength) {
+        case 0:
+            return 'no one likes this';
+            break;
+        case 1:
+            return `${arr[0]} likes this`;
+            break;
+        case 2:
+            return `${arr[0]} and ${arr[1]} like this`;
+            break;
+        case 3:
+            return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+            break;
+        default:
+            return `${arr[0]}, ${arr[1]} and ${arrLength - 2} others like this`;
+            break;
+
+    }
+}
 
 module.exports = displayLikes;
