cfafee705f9eba3db765638c1ce0b80916cb1457
Plamen Petrov <biznetmen@gmail.com>
2023-10-10 11:00:40 +0300

    run 02-08

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,76 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+    let resultArr = [];
+    for (let i = 1; i <= num; i++) {
+        let dice1 = Math.round(Math.random() * 5 + 1);
+        let dice2 = Math.round(Math.random() * 5 + 1);
+        let sum = dice1 + dice2;
+
+        switch (sum) {
+            case 2:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'lose'
+                    }
+                );
+                break;
+            case 3:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'lose'
+                    }
+                );
+                break;
+            case 7:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'win'
+                    }
+                );
+                break;
+            case 11:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'win'
+                    }
+                );
+                break;
+            case 12:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'lose'
+                    }
+                );
+                break;
+            default:
+                resultArr.push(
+                    {
+                        dice1: dice1,
+                        dice2: dice2,
+                        sum: sum,
+                        result: 'roll again'
+                    }
+                );
+                break;
+
+        }
+    }
+
+    return resultArr;
+}
 
 module.exports = diceGameSimulation;
