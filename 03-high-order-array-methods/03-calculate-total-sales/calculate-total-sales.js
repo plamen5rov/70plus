b980f85a6ae30ca85080d4e854c364cd760db419
Plamen Petrov <biznetmen@gmail.com>
2023-10-12 11:21:36 +0300

    run 03-03

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+    const totalSales = products.map(product => product.price * product.quantity)
+        .map(sale => (sale + sale * (tax / 100)))
+        .reduce((sum, sale) => sum + sale, 0);
+
+    return totalSales;
+}
 
 module.exports = calculateTotalSalesWithTax;
