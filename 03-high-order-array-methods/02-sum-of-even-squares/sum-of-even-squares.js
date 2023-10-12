ea30b8724acc5e229f8cba5528d3ceb7583b4a4c
Plamen Petrov <biznetmen@gmail.com>
2023-10-12 11:07:18 +0300

    run 03-02

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    const evenNumbers = arr.filter((num) => num % 2 === 0);
+    const squareNumbers = evenNumbers.map((num) => num * num);
+    const sum = squareNumbers.reduce((sum, num) => sum + num, 0);
+
+    return sum;
+}
 
 module.exports = sumOfEvenSquares;
