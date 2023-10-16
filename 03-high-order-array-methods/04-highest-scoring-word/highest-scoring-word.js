cbea8436d4354f8e64474198d4b54a0bad7f0f1a
Plamen Petrov <biznetmen@gmail.com>
2023-10-16 10:48:04 +0300

    run 03-04

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,18 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+    const wordsArray = str.split(' ');
+    let arrSum = [];
+    let sum = 0;
+    wordsArray.forEach(word => {
+        for (let i = 0; i < word.length; i++) {
+            sum = sum + (word.charCodeAt(i) - 96);
+        }
+        arrSum.push(sum)
+    });
+
+    const index = arrSum.indexOf(Math.max(...arrSum));
+
+    return wordsArray[index];
+
+}
 
 module.exports = highestScoringWord;
