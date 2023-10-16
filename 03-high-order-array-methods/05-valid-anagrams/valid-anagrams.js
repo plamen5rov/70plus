924a285d4263d00c15aae6967dc7fff8af8f137e
Plamen Petrov <biznetmen@gmail.com>
2023-10-16 11:47:41 +0300

    run 03-05

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,11 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    const arr1 = str1.split('').sort();
+    const arr2 = str2.split('').sort();
+    const word1 = arr1.join('');
+    const word2 = arr2.join('');
+    // console.log(word1, word2);
+
+    return word1 === word2;
+}
 
 module.exports = validAnagrams;
