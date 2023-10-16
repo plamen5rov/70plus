73b40e8f5cda740f144f1b460a6a081d88bbd260
Plamen Petrov <biznetmen@gmail.com>
2023-10-16 11:56:32 +0300

    03-05 refactoring

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,9 +1,6 @@
 function validAnagrams(str1, str2) {
-    const arr1 = str1.split('').sort();
-    const arr2 = str2.split('').sort();
-    const word1 = arr1.join('');
-    const word2 = arr2.join('');
-    // console.log(word1, word2);
+    const word1 = str1.split('').sort().join('');
+    const word2 = str2.split('').sort().join('');
 
     return word1 === word2;
 }
